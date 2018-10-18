d4e43ff3ecf7dff3f520ccbfffa59ddf07e3b611
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-10-18 16:22:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
   end
 
   def match(array)
-    anagrams = array.collect {|w| w.chars.sort.join == self.word}
+    anagrams = array.find_all {|w| w.chars.sort.join == self.word}
     anagrams
   end
 
